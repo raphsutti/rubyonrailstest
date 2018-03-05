f1b25480abdf34d9190354cc8c875fb85494a8e6
raphsutti <neoster9@gmail.com>
2018-03-05 21:40:49 +1100

    Added posting features and display posts on home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # Root
+  root 'posts#index', as: 'home'
+
+  # About
+  get 'about' => 'pages#about', as: 'about'
+
+  
+  resources :posts
 end
